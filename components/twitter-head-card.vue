26863503e16307d5e1bc725cbf7683e9b658d3b8
Matej Nemcek <ybdaba@gmail.com>
2017-07-02 15:27:13 +0200

    init

---
+++ components/twitter-head-card.vue
@@ -0,0 +1,22 @@
+<template>
+  <span v-if="false"></span>
+</template>
+
+<script>
+// Test on: https://cards-dev.twitter.com/validator
+export default {
+  data: () => ({
+    href: 'https://twitter.com/intent/tweet?url=' + encodeURIComponent('https://head-elements.now.sh/about')
+  }),
+  head: {
+    meta: [
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@yangwao' },
+      { name: 'twitter:title', content: 'Crypto2020 project' },
+      { name: 'twitter:description', content: 'Crypto2020 aims to part all banking products, soon' },
+      { name: 'twitter:image', content: 'https://s.gravatar.com/avatar/d31afb47d607e443bbf5f781922407cf?s=80&r=x' },
+      { name: 'twitter:image:alt', content: 'yangwao avatar' }
+    ]
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>💸 Crypto2020 🚀🌴</h1>
+    <h3>🐙 Renounce all classic banking products 🐬</h3>
+    <h3>🐋 Move to completely into cryptocurrencies 🐠</h3>
+    <!-- <nuxt-link to="/">About page</nuxt-link> -->
+    <a href="http://hypersignal.xyz">Hypersignal.xyz</a>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '💸 Crypto2020 🚀',
+    meta: [
+      { hid: 'crypto2020', name: 'crypto2020', content: 'crypto2020' }
+    ]
+  }
+}
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 150px;
+  font-size: 20px;
+}
+</style>
